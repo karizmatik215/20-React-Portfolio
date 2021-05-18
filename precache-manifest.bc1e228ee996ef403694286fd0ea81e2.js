c1d88966e168324d020ccf41dac6b8cc3d41c4e2
Juan Sanchez <juan.sanchez@phila.gov>
2021-05-18 05:55:49 -0400

    Updates

---
+++ precache-manifest.bc1e228ee996ef403694286fd0ea81e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e58dad7b56e19fead0b4",
+    "url": "/20-React-Portfolio/static/js/main.c497e31a.chunk.js"
+  },
+  {
+    "revision": "026e356e30d9a400e817",
+    "url": "/20-React-Portfolio/static/js/runtime~main.d78897a2.js"
+  },
+  {
+    "revision": "4c8fd53b3444efc3d06c",
+    "url": "/20-React-Portfolio/static/js/2.4959dd17.chunk.js"
+  },
+  {
+    "revision": "921197361b709c94bd347f409cb636f9",
+    "url": "/20-React-Portfolio/index.html"
+  }
+];
